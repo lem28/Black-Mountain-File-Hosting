4276a7e5ccd69775b0847fe4b94d8c19451cfb71
Luis Montenegro <lem28@njit.edu>
2016-03-17 17:36:28 -0400

    Update: fixing up index page and upload script

--- php/upload.php
+++ php/upload.php
@@ -1,25 +1,26 @@
 <?php
 require_once("file_db.php");
 
-$uploadpath = 'upload/';									// directory to store the uploaded files
-$max_size = 10;												// maximum file size, in KiloBytes
-$alwidth = 10000;											// maximum allowed width, in pixels
-$alheight = 10000;											// maximum allowed height, in pixels
-$allowtype = array('bmp', 'gif', 'jpg', 'jpeg', 'png');		// allowed extensions
+$uploadpath = 'upload/';
+$max_size = 100;
+$alwidth = 100000;
+$alheight = 100000;
+$allowtype = array('bmp', 'gif', 'jpg', 'jpeg', 'png');
 
 if(isset($_FILES['fileup']) && strlen($_FILES['fileup']['name']) > 1) {
-	$uploadpath = $uploadpath . basename( $_FILES['fileup']['name']);// gets the file name
+	$uploadpath = $uploadpath . basename( $_FILES['fileup']['name']);
 	$sepext = explode('.', strtolower($_FILES['fileup']['name']));
-	$type = end($sepext);// gets extension
-	list($width, $height) = getimagesize($_FILES['fileup']['tmp_name']);// gets image width and height
-	$err = '';// to store the errors
+	$type = end($sepext);
+	list($width, $height) = getimagesize($_FILES['fileup']['tmp_name']);
+	$err = '';
 
-	// Checks if the file has allowed type, size, width and height (for images)
-	if(!in_array($type, $allowtype)) $err .= 'The file: <b>'. $_FILES['fileup']['name']. '</b> not has the allowed extension type.';
-	if($_FILES['fileup']['size'] > $max_size*1000000) $err .= '<br/>Maximum file size must be: '. $max_size. ' MB.';
-	if(isset($width) && isset($height) && ($width >= $alwidth || $height >= $alheight)) $err .= '<br/>The maximum Width x Height must be: '. $alwidth. ' x '. $alheight;
+	if(!in_array($type, $allowtype))
+		$err .= 'The file: <b>'. $_FILES['fileup']['name']. '</b> not has the allowed extension type.';
+	if($_FILES['fileup']['size'] > $max_size*1000000)
+		$err .= '<br/>Maximum file size must be: '. $max_size. ' MB.';
+	if(isset($width) && isset($height) && ($width >= $alwidth || $height >= $alheight))
+		$err .= '<br/>The maximum Width x Height must be: '. $alwidth. ' x '. $alheight;
 
-	// If no errors, upload the image, else, output the errors
 	if($err == '') {
 		if(move_uploaded_file($_FILES['fileup']['tmp_name'], $uploadpath)) { 
 			echo 'File: <b>'. basename( $_FILES['fileup']['name']). '</b> successfully uploaded:';
