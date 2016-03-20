10c94053c1a07b5e115ac1ffe555eb0da5587190
Luis Montenegro <lem28@njit.edu>
2016-03-19 22:51:15 -0400

    Shit's broken Update: trying to fix it

--- rpc.php
+++ rpc.php
@@ -1,33 +1,19 @@
 <?php
-require_once("includes/user.php.inc");
-require_once("includes/file.php.inc");
+require_once("user.php.inc");
+require_once("file.php.inc");
 
 $request = $_POST['request'];
 $response = "FUCK<p>";
 
 switch($request)
 {
-	case "login":
-		$username = $_POST['username'];
-		$password = $_POST['password'];
-		$login = new user_db("../ini/connect.ini");
-		$response = $login->validate_user($username, $password);
-		if ($response['success'])
-		{
-			$response = "Login Successful!<p>";
-		}
-		else		
-		{
-			$response = "Login Failed...<p>";
-		}
-		break;
 	case "register":
 		$username = $_POST['username'];
 		$password = $_POST['password'];
 		$first_name = $_POST['first_name'];
 		$last_name = $_POST['last_name'];
 		$email = $_POST['user_email'];
-		$login = new user_db("../ini/connect.ini");
+		$login = new user("connect.ini");
 		$response = $login->validate_user($username, $password);
 		if ($response['success'])
 		{
@@ -38,13 +24,33 @@ switch($request)
 			$login->add_new_user($username,$password,$first_name,$last_name,$email);
 			$response = "$username Registered Successfully!<p>";
 		}
-		break;	
+		break;
+	case "login":
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		$login = new user("connect.ini");
+		$response = $login->validate_user($username, $password);
+		if ($response['success'])
+		{
+			$response = "Login Successful!<p>";
+		}
+		else		
+		{
+			$response = "Login Failed...<p>";
+		}
+		break;
+	case "upload":
+		$file = $_POST['fileup'];
+		$expire = $POST['expire'];
+		$db = new file("connect.ini");
+		$db->upload($param);
+		break;
 	case "search":
-		$db = new file_db("../ini/connect.ini");
+		$db = new file("connect.ini");
 		$db->search($param);
 		break;
 	case "browse":
-		$db = new file_db("../ini/connect.ini");
+		$db = new file("connect.ini");
 		$db->browse();
 		break;		
 }
